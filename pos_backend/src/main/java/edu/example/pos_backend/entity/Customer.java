e054ca859da32406f52f5b705f9345a26186022a
dadithya01 <dadithya01@gmail.com>
2026-02-18 16:04:19 +0530

    add Customer, Item, Order, and OrderDetail entities with DTOs and validation

--- pos_backend/src/main/java/edu/example/pos_backend/entity/Customer.java
+++ pos_backend/src/main/java/edu/example/pos_backend/entity/Customer.java
@@ -1,4 +1,23 @@
 package edu.example.pos_backend.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String address;
+
+    @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL)
+    private List<Order> orders;
 }
--- pos_backend/src/main/java/edu/example/pos_backend/entity/Order.java
+++ pos_backend/src/main/java/edu/example/pos_backend/entity/Order.java
@@ -1,4 +1,29 @@
 package edu.example.pos_backend.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "orders")
 public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDate date;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    private List<OrderDetail> orderDetails;
 }
